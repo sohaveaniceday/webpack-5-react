cf250fb386d1a52a7d02e0d86ed9920e81c6cc83
Rich <rclt201@gmail.com>
2021-03-29 09:17:48 +0100

    feat: added react and hot reloading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello Project.')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const title = 'React with Webpack and Yo'
+
+ReactDOM.render(<App title={title} />, document.getElementById('app'))
+
+module.hot.accept()
